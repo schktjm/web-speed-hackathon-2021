dc24971c95b6033086d82acb2c8dcddf849f8476
schktjm <si2.o.mjt@gmail.com>
2021-12-09 00:23:07 +0900

    :+1: production の追加

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,3 +1,5 @@
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
   presets: [
     [
@@ -11,7 +13,7 @@ module.exports = {
     [
       '@babel/preset-react',
       {
-        development: true,
+        development: isDev,
       },
     ],
   ],
